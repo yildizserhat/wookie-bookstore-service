4f6ac255d22374112b388de9c82ab67cb6af5b89
serhatyildiz <yildiz_serhat@hotmail.com>
2023-04-03 22:19:53 +0200

    Initial commit

---
+++ src/main/java/com/yildiz/serhat/wookiebookstoreservice/WookieBookstoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yildiz.serhat.wookiebookstoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WookieBookstoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WookieBookstoreServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yildiz/serhat/wookiebookstoreservice/WookieBookstoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yildiz.serhat.wookiebookstoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WookieBookstoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
